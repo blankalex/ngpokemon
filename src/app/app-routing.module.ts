20a8d9f4406d416305707db84e623080c557e44f
Alex Maass Blank - Compasso - TI <alex.blank@compasso.com.br>
2019-09-17 09:24:04 -0300

    Added: Component Content, Component Header, Component Error 404. Updated: App Routing, App Componet, App Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+import { Error404Component } from './error404/error404.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: ContentComponent},
+  {path:'*', component: Error404Component},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HeaderComponent } from "./header/header.component";
+import { ContentComponent } from "./content/content.component";
+import { Error404Component } from "./error404/error404.component";
+import { MatToolbar, MatToolbarModule } from "@angular/material";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
-  providers: [],
+  providers: [MatToolbar],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
